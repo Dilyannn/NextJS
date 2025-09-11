084a7e7149191a33d5ac46a4bc9280d3a1745179
Dilyannn <dilyanpaypal@gmail.com>
2025-09-11 16:14:48 +0300

    Fetch meals data from database in MealsPage
    MealsPage now asynchronously loads meal data from the database using the new getAllMeals function in lib/meals.js, replacing the static empty array. This enables dynamic rendering of meals from the database.

--- app/(pages)/meals/page.js
+++ app/(pages)/meals/page.js
@@ -1,14 +1,17 @@
 import MealsHeader from '@/components/meals/meals-header';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getAllMeals } from '@/lib/meals';
 
 import styles from './page.module.css';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getAllMeals();
+
   return (
     <>
       <MealsHeader />
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );

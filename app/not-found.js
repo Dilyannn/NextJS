4e2998b8a69e0f2d8230750b128aa3fc27e98a48
Dilyannn <dilyanpaypal@gmail.com>
2025-09-11 17:07:15 +0300

    Add NotFound page component

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Sorry, we couldn&#39;t find the page you&#39;re looking for.</p>
+    </main>
+  );
+}

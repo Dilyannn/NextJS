0cbb56d8e84c02f3882ff23df7de4e0ce6c9ab92
Dilyannn <dilyanpaypal@gmail.com>
2025-09-14 13:52:01 +0300

    Add meal details page with styling

--- app/(pages)/meals/[mealSlug]/page.js
+++ app/(pages)/meals/[mealSlug]/page.js
@@ -1,3 +1,28 @@
-export default function MealSlugPage({ params }) {
-  return <div>Meal slug page: {params.mealSlug}</div>
+import Image from 'next/image';
+
+import styles from './page.module.css';
+
+export default function MealsDetailsPage({ params }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>TITLE</h1>
+          <p className={styles.creator}>
+            Created by <a href={`mailto:${`EMAIL`}`}>NAME</a>
+          </p>
+          <p className={styles.summary}>
+            SUMMARY
+          </p>
+        </div>
+      </header>
+
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{ __html: '...' }} ></p>
+      </main>
+    </> 
+  );
 }
